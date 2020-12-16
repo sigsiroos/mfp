cdb5cef6be1544879e5325384e5738f5d95e2c96
Sina Syrous <sinasyrous@iMac-som-tillhor-Sina.local>
2020-12-16 10:38:43 +0100

    Fix routing to work in marketing in isolation and the container

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,23 @@
-import React, { useRef, useEffect } from 'react';
-import { mount } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { mount } from "marketing/MarketingApp";
 
 export default () => {
-    const ref = useRef(null);
+  const ref = useRef(null);
+  const history = useHistory(); //This will be container browser history
 
-    useEffect(() => {
-        mount(ref.current);
-    }, []);
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
 
-    return <div ref={ref} />;
+    history.listen(onParentNavigate);
+  }, []);
+
+  return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
 
 // Mount function to start up the app
-const mount = (el) => {
-    ReactDOM.render(
-        <App />,
-        el
-    );
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate); //Listen to history change and call onNavigate on that change. This is marketings memory history
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        //Avoid inifinte loop which is that containers browser history updates, update the subApp memory history and vice versa
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in dev and in isolation,
-// call mount immediately
-if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+// call mount immediately and provide browser history
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
 
-    if (devRoot) {
-        mount(devRoot);
-    }
-};
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
 
 export { mount };
