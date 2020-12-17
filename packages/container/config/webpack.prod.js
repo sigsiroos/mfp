6f23359b11c33f13257d6260951f018da7481d27
Sina Syrous <sinasyrous@iMac-som-tillhor-Sina.local>
2020-12-17 14:50:36 +0100

    webpack config prod for container

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,6 +16,7 @@ const prodConfig = {
             name: 'coontainer',
             remotes: {
                 marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+                auth: `auth@${domain}/auth/latest/remoteEntry.js`,
             },
             shared: packageJson.dependencies,
         }),
